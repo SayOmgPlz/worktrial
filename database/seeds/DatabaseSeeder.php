6bb5f268b942794874940c0f04fb6304bfe44d32
Lutchezar Canov <lecanov@abv.bg>
2015-04-21 11:19:43 +0300

    1. User seeds 2. routes.php - homepage should be login

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Worktrial\User;
 
 class DatabaseSeeder extends Seeder {
 
@@ -14,7 +15,16 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
 	}
 
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run() {
+        DB::table("users")->delete();
+
+        User::create(['email' => 'test@test.com', 'password'=> bcrypt('test'), 'name'=> 'test']);
+    }
 }
