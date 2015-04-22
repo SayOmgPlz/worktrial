41104cbc419f4d77237fa329c3682f0ce758a695
Lutchezar Canov <lecanov@abv.bg>
2015-04-22 11:59:49 +0300

    View for the modal form and style changes.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,14 @@
 @section('content')
 <div class="container">
 	<div class="row">
-		<div class="col-md-12 task-list-container">
+		<div class="col-md-12 task-list-container" id="container-task-list">
             <div class="panel panel-default">
                 <div class="panel-heading clearfix" id="task-header" >
                     <a href="javascript:;" class="col-md-2" data-sort="created_at">Created</a>
                     <a href="javascript:;" class="col-md-2" data-sort="owner">Owner</a>
                     <a href="javascript:;" class="col-md-5" data-sort="description">Description</a>
                     <a href="javascript:;" class="col-md-2" data-sort="state">State</a>
+                    <span  class="col-md-1" id="add-new-task"><i class="add-task-icon"> </i> NEW</span>
                 </div>
 
 
@@ -22,5 +23,6 @@
             </div>
 		</div>
 	</div>
+    @include( 'tasks.dialog-box', array('users' => $users) )
 </div>
 @endsection
